a7ea0deced72915026258f7f6c75a910c5dbb0e9
Juan Salazar <jjsc2003@live.com>
2023-03-21 10:07:09 -0600

    Initialized Project and Firebase Connected

---
+++ coffee-tracker/src/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, collection, getDocs } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwW0QqNpjvaxDyxoyflvEr3LOI2HijTgo",
+  authDomain: "tec-week-iot.firebaseapp.com",
+  databaseURL: "https://tec-week-iot-default-rtdb.firebaseio.com",
+  projectId: "tec-week-iot",
+  storageBucket: "tec-week-iot.appspot.com",
+  messagingSenderId: "382023793289",
+  appId: "1:382023793289:web:0aa51482d20468c767a458",
+  measurementId: "G-7C2K0B0XG8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+

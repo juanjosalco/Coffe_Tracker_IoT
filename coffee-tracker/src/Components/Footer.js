f71a7de2c95d2b989089d7125e0446397845789f
NolbertoCastro <nolber0703@gmail.com>
2023-03-21 10:44:22 -0600

    Landing

---
+++ coffee-tracker/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import '../CSS/footer.css';
+
+function Footer() {
+  return (
+    <footer>
+        <img src="../Assets/logo blanco.svg" alt="logo blanco"/>
+        <h3>Copyright ©2022</h3>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
+
+
+
+
